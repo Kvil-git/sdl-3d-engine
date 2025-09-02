d494ce9f51d539476aa3565a64c113b76484762b
Kvil <kiridohard@gmail.com>
2025-09-02 11:40:57 +0200

    add color "enums" (actually it is a namespace with variables)

---
+++ 3D_Rendering/Enums/Colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+#include "../../EngineMath/Vector.h"
+#include "stdint.h"
+
+
+namespace Colors{
+    using Color3 = Vector<uint8_t, 3>;
+    const Color3 Red({255, 0, 0});
+    const Color3 Green({0, 255, 0});
+    const Color3 Blue({0, 255, 0});
+    const Color3 White({255, 255, 255});
+    const Color3 Black({255, 255, 255});
+};
+
+
+#endif
