1be0a175558dc8f48a3f903f11dc9c9c0b09e13c
Kvil <kiridohard@gmail.com>
2025-09-02 11:43:45 +0200

    forgot to change green and black after ctrl+c-ing them

--- 3D_Rendering/Enums/Colors.h
+++ 3D_Rendering/Enums/Colors.h
@@ -8,9 +8,9 @@ namespace Colors{
     using Color3 = Vector<uint8_t, 3>;
     const Color3 Red({255, 0, 0});
     const Color3 Green({0, 255, 0});
-    const Color3 Blue({0, 255, 0});
+    const Color3 Blue({0, 0, 255});
     const Color3 White({255, 255, 255});
-    const Color3 Black({255, 255, 255});
+    const Color3 Black({0, 0, 0});
 };
 
 
