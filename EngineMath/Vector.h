1bf7c1a1544559f30e41d93c56511945e4f3e97b
Kvil <kiridohard@gmail.com>
2025-09-07 13:25:47 +0200

    refactor vector (added const modifiers to functions, changed some names, etc)

--- EngineMath/Vector.h
+++ EngineMath/Vector.h
@@ -38,7 +38,7 @@ struct Vector {
 
 
     // matrix-vector multiplication
-    template<typename MatrixType> VectorN operator*(const Matrix<MatrixType, Dimensions, Dimensions> &matrix) {
+    template<typename MatrixType> VectorN operator*(const Matrix<MatrixType, Dimensions, Dimensions> &matrix) const {
         static_assert(Dimensions > 0, "Matrix-vector multiplication is not defined for zero-dimensional vectors.");
         VectorN temp;
         for(int i=0; i < Dimensions; i++) {
@@ -61,9 +61,9 @@ struct Vector {
         *this = temp;
     }
 
-    // Apply a rotation stored in unit quaternion to a 3D vector
+    //Apply a rotation stored in unit quaternion to a 3D vector
     template<typename QuaternionType>
-    Vector3 RotateByQuaternion(const QuaternionType &quaternion) {
+    void RotateByQuaternion(const QuaternionType &quaternion) {
         static_assert(Dimensions == 3, "Rotation by quaternion is only defined for 3D vectors.");
 
         QuaternionType q = quaternion;
@@ -73,27 +73,40 @@ struct Vector {
 
         QuaternionType result = q * p * q_conjugate;
 
-        return Vector3({result.components[1], result.components[2], result.components[3]});
+        *this = Vector3(result.components[1], result.components[2], result.components[3]);
+    }
+
+    // Make a copy rotated by a quaternion
+    template<typename QuaternionType>
+    Vector3 CopyRotatedByQuaternion(const QuaternionType &quaternion) const {
+        static_assert(Dimensions == 3, "Rotation by quaternion is only defined for 3D vectors.");
+
+        QuaternionType q = quaternion;
+        QuaternionType q_conjugate = {q.components[0], -q.components[1], -q.components[2], -q.components[3]};
+
+        QuaternionType p = {0, components[0], components[1], components[2]};
+
+        QuaternionType result = q * p * q_conjugate;
+
+        return Vector3(result.components[1], result.components[2], result.components[3]);
     }
 
     // change all components of a vector to coresponding array element values
-    VectorN operator=(const ComponentType array[Dimensions]) {
-        VectorN output;
+    void operator=(const ComponentType array[Dimensions]) {
         for(int i=0; i<Dimensions; i++){
-            output.components[i] = array[i];
+            components[i] = array[i];
         }
-        return output;
     }
 
     // change all components of a vector to coresponding components of another vector
     void operator=(const VectorN& other) {
         for(int i=0; i<Dimensions; i++){
-            this->components[i] = other.components[i];
+            components[i] = other.components[i];
         }
     }
 
     // vector addition just means adding corresponding coords 
-    Vector operator+(const VectorN &other) {
+    Vector operator+(const VectorN &other) const {
         static_assert(Dimensions > 0, "Vector addition is not defined for zero-dimensional vectors.");
         Vector<ComponentType,Dimensions> temp;
         for(int i=0; i < Dimensions; i++) temp.components[i] = components[i] + other.components[i];
@@ -115,13 +128,13 @@ struct Vector {
     }
 
     // actual vector subtraction is basically addition of a negated vector
-    Vector operator-(const VectorN &other) {
+    Vector operator-(const VectorN &other) const {
         static_assert(Dimensions > 0, "Vector subtraction is not defined for zero-dimensional vectors.");
         return (*this) + (-other);
     }
 
     // vector scaling
-    template<typename scalarType> VectorN operator*(const scalarType &scalar) {
+    template<typename scalarType> VectorN operator*(const scalarType &scalar) const {
         static_assert(Dimensions > 0, "Vector scaling is not defined for zero-dimensional vectors.");
         VectorN temp;
         for(int i=0; i < Dimensions; i++) temp.components[i] = components[i] * scalar;
@@ -135,7 +148,7 @@ struct Vector {
     }
 
     // vector scaling 2
-    template<typename scalarType> Vector<ComponentType,Dimensions> operator/(const scalarType &scalar) {
+    template<typename scalarType> Vector<ComponentType,Dimensions> operator/(const scalarType &scalar) const {
         static_assert(Dimensions > 0, "Vector scaling is not defined for zero-dimensional vectors.");
         VectorN temp;
         for(int i=0; i < Dimensions; i++) temp.components[i] = components[i] / scalar;
@@ -149,33 +162,33 @@ struct Vector {
     }
 
     // vector length and magnitude are the same thing idgaf
-    double Length() {
+    double Length() const {
         static_assert(Dimensions > 0, "Length is not defined for zero-dimensional vectors.");
         return sqrt(this->SquaredComponentSum());
     }
 
-    double Mangitude() {
+    double Mangitude() const {
         return this->Length();
     }
 
     // sum of a vector's squared components is its length squared
-    ComponentType SquaredComponentSum() {
+    ComponentType SquaredComponentSum() const {
         static_assert(Dimensions > 0, "Squared component sum is not defined for zero-dimensional vectors.");
         ComponentType squaredComponentSum = 0;
         for(int i=0; i < Dimensions; i++) squaredComponentSum += components[i] * components[i];
         return squaredComponentSum;
     }
 
-    ComponentType LengthSquared() {
+    ComponentType LengthSquared() const {
         return this->SquaredComponentSum();
     }
 
-    ComponentType MangitudeSquared() {
+    ComponentType MangitudeSquared() const {
         return this->SquaredComponentSum();
     }
 
     // dot product of two vectors
-    ComponentType operator*(const VectorN &other) {
+    ComponentType operator*(const VectorN &other) const {
         static_assert(Dimensions > 0, "Dot product is not defined for zero-dimensional vectors.");
         size_t size = Dimensions;
         ComponentType dotProduct = 0;
@@ -184,7 +197,7 @@ struct Vector {
     }
 
     // cross product
-    Vector3 operator%(const Vector<ComponentType,Dimensions> &other) {
+    Vector3 operator%(const Vector<ComponentType,Dimensions> &other) const {
         static_assert(Dimensions == 3, "Cross product is only defined for 3D vectors.");
         
         ComponentType v1 = components[0];
@@ -198,14 +211,14 @@ struct Vector {
         return Vector3({v2*w3 - v3*w2, v3*w1 - v1*w3, v1*w2 - v2*w1});
     }
 
-    ComponentType ComponentSum() {
+    ComponentType ComponentSum() const {
         static_assert(Dimensions > 0, "Component sum is not defined for zero-dimensional vectors.");
         ComponentType sum = 0;
         for(int i=0; i < Dimensions; i++) sum += components[i];
         return sum;
     }
 
-    inline bool IsZero(){
+    inline bool IsZero() const {
         return this->ComponentSum() == 0;
     }
 
@@ -220,7 +233,7 @@ struct Vector {
     }    
 
     // return a normalized copy of this vector
-    VectorN Unit() {
+    VectorN Unit() const {
         static_assert(Dimensions > 0, "Unit vector is not defined for zero-dimensional vectors.");
         double len = Length();
         if (len < 1e-10) {
